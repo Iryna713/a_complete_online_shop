02f894933996928c75a7512fcce082b7b8b51200
Iryna Kozhura <ivladimirovna713@gmail.com>
2026-02-10 12:54:49 +0100

    basic sceleton and styles added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const path = require("path");
 const express = require("express");
 
 const authRotes = require("./routes/auth.routes");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
 
 app.use(authRotes); // with 'use' method we add middleware that will be triggered for every incoming request
 
